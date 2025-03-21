2ceb096c998eea2328b972a86b01f53d8fd4b04f
OscarMarcusson <oscar.marcusson@outlook.com>
2025-03-21 10:11:14 +0100

    Add input reading and direct copying to output

--- src/main.c
+++ src/main.c
@@ -13,5 +13,44 @@ int main(const int argc, const char* argv[]) {
 		return 1;
 	}
 
-	return 0;
+	FILE* src = fopen(args.input, "r");
+	if (src == NULL) {
+		fail("Could not find " FPATH, args.input);
+		return 1;
+	}
+
+	FILE* out = fopen(args.output, "w");
+	if (out == NULL) {
+		fclose(src);
+		fail("Failed to create " FPATH, args.output);
+		return 1;
+	}
+
+	const int max = 1023;
+	char buffer[max + 1];
+	buffer[max] = '\0';
+
+	int i = 0;
+	int c;
+	bool success = true;
+	while ((c = fgetc(src)) >= 0) {
+		buffer[i] = c;
+		i++;
+		if (i == max) {
+			if (fputs(buffer, out) == EOF) {
+				success = false;
+				break;
+			}
+			i = 0;
+		}
+	}
+	if (i > 0) {
+		buffer[i] = '\0';
+		if (fputs(buffer, out) == EOF) success = false;
+	}
+
+
+	fclose(src);
+
+	return success ? 0 : 1;
 }
