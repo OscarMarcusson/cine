f0d267fac1dcf4d0e190fac5e4f1a669a71de7ae
OscarMarcusson <oscar.marcusson@outlook.com>
2025-03-22 09:00:01 +0100

    Add basic parsing with a todo for the includes to resolve

--- src/main.c
+++ src/main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "logging.h"
 #include "args.h"
+
+// Returns false if the buffer ran out
+static bool fread_to_whitespace(FILE* file, char* buffer, int max) {
+	int c = 0;
+	int i = 0;
+	while ((c = fgetc(file)) >= 0) {
+		if (i >= max) {
+			buffer[max] = '\0';
+			return false;
+		}
+		if (c == '\r' || c == '\n' || c == ' ' || c == '\t') break;
+		buffer[i] = c;
+		i++;
+	}
+	buffer[i] = '\0';
+	return i;
+}
+
+// Returns the length
+static int get_directive(FILE* file, char* buffer, int max) {
+	int c = 0;
+	int i = 0;
+	while ((c = fgetc(file)) >= 0 && i < max) {
+		if (c == '\r' || c == '\n') break;
+		buffer[i] = c;
+		i++;
+	}
+	buffer[i] = '\0';
+	return i;
+}
+
 
 int main(const int argc, const char* argv[]) {
 	if (argc == 1) {
@@ -30,18 +62,84 @@ int main(const int argc, const char* argv[]) {
 	char buffer[max + 1];
 	buffer[max] = '\0';
 
+	char dbuffer[max + 1];
+	dbuffer[max] = '\0';
+
 	int i = 0;
 	int c;
 	bool success = true;
+	char end = '\0';
+
+	// Helper
+#   define flush(text) if (fputs(text, out) == EOF) \
+	{\
+		success = false;\
+		break;\
+	}
+
+	// Flushes the main buffer and restarting the counter
+#   define flush_main() buffer[i] = '\0'; flush(buffer); i = 0;
+
+#   define push(char_value) buffer[i] = char_value; i++;
+
+
+	// A fail means the input was massive, and not workable. So just dump it into the file and continue
+#   define load_next_word_into_dbuffer() if (!fread_to_whitespace(src, dbuffer, max + 1)) {\
+		push('#'); \
+		flush_main(); \
+		flush(dbuffer); \
+		push(' '); \
+		continue; \
+	}
+
+	// Read input
 	while ((c = fgetc(src)) >= 0) {
-		buffer[i] = c;
-		i++;
-		if (i == max) {
-			if (fputs(buffer, out) == EOF) {
-				success = false;
-				break;
+		// Inside a string
+		if (end != '\0') {
+			if (buffer[i - 1] != '\\' && c == end) {
+				end = '\0';
 			}
-			i = 0;
+			push(c);
+		}
+		else if (c == '#') {
+			load_next_word_into_dbuffer();
+			if (strcmp("include", dbuffer) == 0) {
+				load_next_word_into_dbuffer();
+				if (dbuffer[0] == '<') {
+					flush_main();
+					push('#');
+					push('i');
+					push('n');
+					push('c');
+					push('l');
+					push('u');
+					push('d');
+					push('e');
+					push(' ');
+					flush_main();
+					flush(dbuffer);
+					push('\n');
+				}
+				else {
+					todo("Handle includes: " ARGF, dbuffer);
+				}
+			}
+			// Some unknown directive, so ignore since we only care about the includes
+			else {
+				push('#');
+				flush_main();
+				flush(dbuffer);
+				push(' ');
+				// buffer[i] = '?';
+				// i++;
+			}
+		}
+		else {
+			push(c);
+		}
+
+		if (i == max) {
+			flush_main();
 		}
 	}
 	if (i > 0) {
